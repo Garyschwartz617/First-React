b5577cdd443d106b96ee485d9aea42fa20fbcf5e
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-10-12 10:20:01 +0300

    multiple props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,12 +7,25 @@ const Home = () => {
     const [name, setName] =useState('Gary');
     const [age, setAge] =useState(25);
 
-    const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    const [blogs,setBlogs] = useState(null)
+
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id != id);
+    //     setBlogs(newBlogs);
+    // }
     
-    ])
+    useEffect(() =>{
+        fetch('http://localhost:8000/blogs')
+        .then(res =>{
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data)
+        })
+        console.log('use effect ran')
+        console.log(name)
+    }, []);
 
     const handleClick = () => {
         setName('Adina');
@@ -23,14 +36,18 @@ const Home = () => {
 
     const handleClick2 = (name, e) => {
         console.log('yo ' + name, e.target)
+        setName('Gary');
+        setAge(25);
+
     }
 
+    
 
     return ( 
         <div className="home">
-                <BlogList blogs={blogs} title = "All Blogs!"/>
-                <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Marios' blog!"/>
- 
+                {blogs &&<BlogList blogs={blogs} title = "All Blogs!" />}
+                {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Marios' blog!"/>}
+            <button onClick={() => setName('luigi')}>Change name</button>
             <p>{name} is { age}  years old</p>
             <button onClick={handleClick}> Click me</button>
             <button onClick={(e) => handleClick2('Gary', e)}> Click me again</button>
