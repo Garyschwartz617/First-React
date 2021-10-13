5a89902c3e5caf2720f78b5cc45e80c3c4c4b122
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-10-13 10:48:09 +0300

    Router/routes

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import BlogList from "./BlogList";
 import useFetch from "./usefetch";
 
 const Home = () => {
-    
-    const { data: blogs,isPending,error} = useFetch('http://localhost:8000/blogs')
+
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     // let name = 'Gary';
-    const [name, setName] =useState('Gary');
-    const [age, setAge] =useState(25);
+    const [name, setName] = useState('Gary');
+    const [age, setAge] = useState(25);
 
-  
+
     // const handleDelete = (id) => {
     //     const newBlogs = blogs.filter(blog => blog.id != id);
     //     setBlogs(newBlogs);
     // }
-    
+
 
     const handleClick = () => {
         setName('Adina');
@@ -31,19 +31,19 @@ const Home = () => {
 
     }
 
-    return ( 
+    return (
         <div className="home">
-            { error && <div>{error}</div>} 
-            { isPending && <div>Loading...</div>}    
-            {blogs &&<BlogList blogs={blogs} title = "All Blogs!" />}
-            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Marios' blog!"/>}
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Marios' blog!" />}
             <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name} is { age}  years old</p>
+            <p>{name} is {age}  years old</p>
             <button onClick={handleClick}> Click me</button>
             <button onClick={(e) => handleClick2('Gary', e)}> Click me again</button>
 
         </div>
-     );
+    );
 }
- 
+
 export default Home;
